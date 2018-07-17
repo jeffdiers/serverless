9c6214a21ca54c4d8a4475fa219338d0a41ff9db
jeffdiers <jeff.diers@gmail.com>
2018-07-17 09:49:45 -0600

    test build

--- upload-build.py
+++ upload-build.py
@@ -7,25 +7,39 @@ import mimetypes
 def lambda_handler(event, context):
     sns = boto3.resource('sns')
     topic = sns.Topic('arn:aws:sns:us-east-1:849548794507:deploy_jeffdiers')
-    
+
+    location = {
+      'bucketName': 'build.jeffdiers.com',
+      'objectKey': 'buildJeffdiers.zip'
+    }
     try:
+        job = event.get('CodePipeline.job')
+
+        if job:
+          for artifact in job['data']['inputArtifacts']:
+            if artifact['name'] == 'MyAppBuild':
+              location = artifact['location']['s3Location']
+
+        print 'Building from ' + str(location)
         s3 = boto3.resource('s3')
         
         jeffdiers_bucket = s3.Bucket('jeffdiers.com')
-        build_bucket = s3.Bucket('build.jeffdiers.com')
+        build_bucket = s3.Bucket(location['bucketName'])
         
         build_zip = StringIO.StringIO()
-        build_bucket.download_fileobj('buildJeffdiers.zip', build_zip)
+        build_bucket.download_fileobj(location['objectKey'], build_zip)
         
         with zipfile.ZipFile(build_zip) as myzip:
             for nm in myzip.namelist():
                 obj = myzip.open(nm)
-                build_bucket.upload_fileobj(obj, nm, 
-                  ExtraArgs={'ContentType': str(mimetypes.guess_type(nm)[0])})
-                build_bucket.Object(nm).Acl().put(ACL='public-read')
+                jeffdiers_bucket.upload_fileobj(obj, nm)
+                jeffdiers_bucket.Object(nm).Acl().put(ACL='public-read')
                 
         print 'Done!' 
         topic.publish(Subject="Jeffdiers.com deploy", Message="it deployed, cunt")
+        if job:
+          codepipeline = boto3.client('codepipeline')
+          codepipeline.put_job_success_result(jobId=job['id'])
     except:
         topic.publish(Subject="Jeffdiers.com deploy", Message="didn't deploy, cunt")
         raise       
