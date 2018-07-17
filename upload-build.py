047b893df93d104ea7fc97439e3254c4aa75e36f
jeffdiers <jeff.diers@gmail.com>
2018-07-16 23:26:14 -0600

    publish sns

--- upload-build.py
+++ upload-build.py
@@ -1,24 +1,33 @@
 import boto3
+import boto3
 import zipfile
 import StringIO
 import mimetypes
 
 def lambda_handler(event, context):
-
-    s3 = boto3.resource('s3')
+    sns = boto3.resource('sns')
+    topic = sns.Topic('arn:aws:sns:us-east-1:849548794507:deploy_jeffdiers')
     
-    jeffdiers_bucket = s3.Bucket('jeffdiers.com')
-    build_bucket = s3.Bucket('build.jeffdiers.com')
-    
-    build_zip = StringIO.StringIO()
-    build_bucket.download_fileobj('buildJeffdiers.zip', build_zip)
-    
-    with zipfile.ZipFile(build_zip) as myzip:
-        for nm in myzip.namelist():
-            obj = myzip.open(nm)
-            build_bucket.upload_fileobj(obj, nm, 
-              ExtraArgs={'ContentType': str(mimetypes.guess_type(nm)[0])}
-            )
-            build_bucket.Object(nm).Acl().put(ACL='public-read')
+    try:
+        s3 = boto3.resource('s3')
+        
+        jeffdiers_bucket = s3.Bucket('jeffdiers.com')
+        build_bucket = s3.Bucket('build.jeffdiers.com')
+        
+        build_zip = StringIO.StringIO()
+        build_bucket.download_fileobj('buildJeffdiers.zip', build_zip)
+        
+        with zipfile.ZipFile(build_zip) as myzip:
+            for nm in myzip.namelist():
+                obj = myzip.open(nm)
+                build_bucket.upload_fileobj(obj, nm, 
+                  ExtraArgs={'ContentType': str(mimetypes.guess_type(nm)[0])})
+                build_bucket.Object(nm).Acl().put(ACL='public-read')
+                
+        print 'Done!' 
+        topic.publish(Subject="Jeffdiers.com deploy", Message="it deployed, cunt")
+    except:
+        topic.publish(Subject="Jeffdiers.com deploy", Message="didn't deploy, cunt")
+        raise       
             
-    return 'Built'
+    return 'Built project'
